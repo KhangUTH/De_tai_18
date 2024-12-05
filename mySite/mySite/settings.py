fc3702a6504d88984cd7067ec65590bdc94aaa2d
KhangUTH <khangtm2739@ut.edu.vn>
2024-12-05 09:50:12 +0700

    ip address

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^n#h+)s3@5mhdmv$00bwm+%2l(e@@a2*hs((91xw5)6&r=y0s='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
