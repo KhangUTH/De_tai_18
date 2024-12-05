d7395e946fac5dc1dac59800f5c209232e19861e
KhangUTH <khangtm2739@ut.edu.vn>
2024-12-05 10:35:52 +0700

    up to local-web

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from DLSCL import views as home
 
 urlpatterns = [
-    path('DLSCL/', include('DLSCL.urls')),
     path('admin/', admin.site.urls),
+    path('',home.get_home)
 ]
